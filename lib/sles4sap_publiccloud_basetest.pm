36333ea404906dadef86313d9d736f74b01f8f43
lpalovsky <lpalovsky@suse.com>
2022-09-30 13:00:20 +0200

    Cleanup routine

---
+++ lib/sles4sap_publiccloud_basetest.pm
@@ -0,0 +1,50 @@
+package sles4sap_publiccloud_basetest;
+
+use base 'publiccloud::basetest';
+use strict;
+use warnings FATAL => 'all';
+use Exporter 'import';
+use testapi;
+use sles4sap_publiccloud;
+use qesapdeployment;
+use Data::Dumper;
+
+our @EXPORT = qw(cleanup);
+
+sub cleanup {
+    my ($self) = @_;
+    # Do not run destroy if already executed
+    return if ($self->{cleanup_called});
+    $self->{cleanup_called} = 1;
+    my $inventory_check_cmd = join(" ", ("test", "-f", qesap_get_inventory()));
+    if (script_run($inventory_check_cmd) == 0){
+        record_info("Ansible cleanup");
+        my $ansible_cleanup_rc = qesap_execute(verbose => "--verbose", cmd => "ansible", cmd_options => "-d", timeout => 1200);
+        record_soft_failure("Ansible destroy failed.") if $ansible_cleanup_rc != 0;
+    }
+
+    record_info("Cleaning up terraform infrastructure");
+    my $terraform_cleanup_rc = qesap_execute(verbose => "--verbose", cmd => "terraform", cmd_options => "-d", timeout => 1200);
+    record_soft_failure("Terraform destroy failed.") if $terraform_cleanup_rc != 0;
+    record_info("Cleanup finished");
+}
+
+sub post_fail_hook {
+    my ($self,) = @_;
+    if (get_var("PUBLIC_CLOUD_NO_CLEANUP_ON_FAILURE")){
+        diag("Skip post fail", "Variable 'PUBLIC_CLOUD_NO_CLEANUP_ON_FAILURE' defined.");
+        return;
+    }
+    $self->cleanup();
+}
+
+sub post_run_hook {
+    my ($self) = @_;
+    if ($self->test_flags()->{publiccloud_multi_module} or get_var("PUBLIC_CLOUD_NO_CLEANUP")){
+        diag("Skip post run", "Skipping post run hook. \n Variable 'PUBLIC_CLOUD_NO_CLEANUP' defined or test_flag 'publiccloud_multi_module' active");
+        return;
+    }
+    $self->cleanup();
+}
+
+1;
---
+++ tests/sles4sap/publiccloud/hana_sr_takeover.pm
@@ -0,0 +1,55 @@
+package hana_sr_takeover;
+use base 'sles4sap_publiccloud_basetest';
+use strict;
+use warnings FATAL => 'all';
+use testapi;
+use Data::Dumper;
+
+sub test_flags {
+    return {fatal => 1, publiccloud_multi_module => 1};
+}
+
+sub run {
+    my ($self, $run_args) = @_;
+    $self->select_serial_terminal;
+    $self->{instances} = $run_args->{instances};
+    my $test_name = $self->{name};
+    my $takeover_action = $run_args->{hana_test_definitions}{$test_name}{action};
+    my $site_name = $run_args->{hana_test_definitions}{$test_name}{site_name};
+    my $target_site = $run_args->{$site_name};
+
+    # Switch to control to target site (currently PROMOTED)
+    $self->{my_instance} = $target_site;
+
+    # Check initial cluster status
+    my $cluster_status = $self->run_cmd(cmd => "crm status");
+    record_info("Cluster status", $cluster_status);
+    # Check initial state: 'site A' = primary mode
+    die(uc($site_name) . " '$target_site->{instance_id}' is NOT in MASTER mode.") if
+      $self->get_promoted_hostname() ne $target_site->{instance_id};
+    record_info(ucfirst($takeover_action) . " DB",
+        join(" ", ucfirst($takeover_action) . "DB on", ucfirst($site_name), "('", $target_site->{instance_id}, "')")
+    );
+
+    # Stop/kill/crash HANA DB and wait till SSH is again available with pacemaker running.
+    # Setup sbd delay in case of crash OS to prevent cluster starting too quickly after reboot
+    $self->setup_sbd_delay("30s") if $takeover_action eq "crash";
+    $self->stop_hana(method => $takeover_action);
+    $self->{my_instance}->wait_for_ssh(username => 'cloudadmin');
+    sleep 10;
+    $self->wait_for_pacemaker();
+
+
+    record_info("Takeover check");
+    $self->check_takeover;
+
+    record_info("Replication", join(" ", ("Enabling replication on", ucfirst($site_name), "(DEMOTED)")));
+    $self->enable_replication();
+
+    record_info(ucfirst($site_name) . " start");
+    $self->cleanup_resource();
+
+    record_info("Done", "Test finished");
+}
+
+1;
